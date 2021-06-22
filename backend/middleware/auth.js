8cd88779c69eefc924c41d2fb314aed2dc84b89e
Guerman <guerman.rouban@gmail.com>
2021-06-22 14:32:04 +0200

    correction dans auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,8 +4,8 @@ module.exports = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(' ') [1];
         const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-        const userID = decodedToken.userID;
-        if (req.body.userID && req.body.userID !== userId) {
+        const userId = decodedToken.userId;
+        if (req.body.userId && req.body.userId !== userId) {
             throw 'User ID non valable !';
         }else{
             next ();
