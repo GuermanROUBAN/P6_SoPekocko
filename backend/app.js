68f3b370007027c18c7123d86b23a5a15b4d6c36
Guerman <guerman.rouban@gmail.com>
2021-06-22 13:44:12 +0200

    upgrade  comments de app.js

--- backend/app.js
+++ backend/app.js
@@ -1,40 +1,72 @@
+// Importation de modules
 
-const express = require('express');
+//Ajout du router express
+const express = require('express'); 
 //-----------------------------------------------------------------------------
+//Ajout du body-parser : permet d'extraire des objets JSON
 const bodyParser = require('body-parser');
 //-----------------------------------------------------------------------------
+// Ajout de mangoose : permet la gestion de BDD
 const mongoose = require('mongoose');
 //-----------------------------------------------------------------------------
-const path = require ('path'); // donne accés au chemin de notre système de fichiers
+// Gestion du système de fichier : donne accès au chemin de notre système de fichiers
+const path = require('path');
+//-----------------------------------------------------------------------------
+// Protection de l'application avec helmet
+// const helmet =  require('helmet');
+//-----------------------------------------------------------------------------
+// Protection contre les attaques des pollution des paramètres HTPP
+//const hpp = require ('hpp');
 
 
+// Importation des routes
 const sauceRoutes = require('./routes/sauce');
 //-----------------------------------------------------------------------------
 const userRoutes = require('./routes/user');
+//-----------------------------------------------------------------------------
 
+// Connexion à MangoDB
 mongoose.connect('mongodb+srv://Redgogo:Test06220@cluster0.lix0p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+//-----------------------------------------------------------------------------
 
-
+// Application express
 const app = express();
 //-----------------------------------------------------------------------------
-//ETAPE 5 / METTRE EN PLACE MANGO DB.
+
+//Correction des erreurs CORS.
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
+//-----------------------------------------------------------------------------
 
-app.use(bodyParser.json()); // Il va transformer le corps de la requete en objet utilisable
+// Utilisation de bodyParser dans l'application
+// Il va transformer le corps de la requete en objet utilisable
+app.use(bodyParser.json());
+//-----------------------------------------------------------------------------
 
+// Protection de l'application avec helmet
+//app.use(helmet());
+//-----------------------------------------------------------------------------
+
+// Utisation de hpp
+//app.use(hpp());
+//-----------------------------------------------------------------------------
+
+// Utilisation du path pour enregistrement des photos sur le BE
 app.use('/images', express.static(path.join(__dirname, 'images')));
+//-----------------------------------------------------------------------------
 
+// Routes
 // on enregistre nos routeurs dans notre application
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
+//-----------------------------------------------------------------------------
