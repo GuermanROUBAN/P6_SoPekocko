a1444406d07a13ec9248c13156e1517ed54cc8c3
Guerman <guerman.rouban@gmail.com>
2021-06-27 17:45:27 +0200

    modif Token

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -42,7 +42,7 @@ exports.login = (req, res, next) => {
                         userId: user._id,
                         token: jwt.sign(
                             { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
+                            `${process.env.TOKEN}`,
                             { expiresIn: '24h' }
                         )
                     });
